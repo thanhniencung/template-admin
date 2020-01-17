1c19ec819597185631ee2ecd8126160a0816b3a2
Kien Nguyen <ryan@exgo.vn>
2020-01-17 17:43:40 +0700

    first commit

---
+++ src/js/app.js
@@ -0,0 +1,64 @@
+$(() => {
+
+
+    $('#version').text('v1.0.3');
+
+    const psSidebar = new PerfectScrollbar('.menubar-body', {
+        suppressScrollX: true
+    });
+
+
+
+    // Showing sub menu in sidebar
+    $('.menu-link[data-toggle="collapse"]').on('click', function (e) {
+        // e.preventDefault();
+        // $(this).parent().toggleClass('show');
+
+        psSidebar.update();
+    });
+
+
+    // 
+
+    $('#navbar-toggler').on('click', function () {
+
+        $('body').toggleClass('menubar-collapsed');
+
+        if ($('body').hasClass("menubar-collapsed")) {
+
+            $('.menubar').on('mouseover').hover(function () {
+                $('body').toggleClass('menubar-collapsed');
+            });
+
+        } else {
+            $('.menubar').unbind();
+        }
+
+
+    });
+
+    $('a[data-toggle=collapse]').on('click', function () {
+        $(this).toggleClass('active');        
+    });
+
+
+    // show source code 
+
+    $('#modal-code').on('show.bs.modal', function (event) {
+        var button = $(event.relatedTarget); // Button that triggered the modal
+        var content = $(button.data('content')).clone().html(); // Extract info from data-* attributes       
+
+        // var code = $('<pre></pre>').text(content);
+        $(this).find('.modal-body').html(content);
+
+    });
+
+
+
+    // Dashboard
+
+
+
+
+
+});
